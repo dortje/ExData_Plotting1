78e62ea52dcc9a793ef5e18a36f478db8fd0920e
Dortje Löper <dortje@turboprinzessin.de>
2017-02-11 17:56:41 +0100

    added some mote comments

--- plot3.R
+++ plot3.R
@@ -13,10 +13,13 @@ source("ExData_Plotting1/read_data.R")
 print("Plotting ...")
 
 png("ExData_Plotting1/plot3.png", width=480, height=480)
+# preparing the plotting area with type = "n" nothing is plotted yet
 plot(household_data$Time, household_data$Sub_metering_1, type="n", ylab="Energy sub metering", xlab="")
+# adding the plot lines for all three sub meterings in different colors
 points(household_data$Time, household_data$Sub_metering_1, type="l", col="black")
 points(household_data$Time, household_data$Sub_metering_2, type="l", col="red")
 points(household_data$Time, household_data$Sub_metering_3, type="l", col="blue")
+# adding a legend 
 legend("topright", col=c("black", "red", "blue"), legend=colnames(household_data)[7:9], lty=1)
 dev.off()
 
--- plot4.R
+++ plot4.R
@@ -15,17 +15,17 @@ print("Plotting ...")
 
 png("ExData_Plotting1/plot4.png", width=480, height=480)
 par(mfrow=c(2,2)) # setting 4 plotting spaces (2x2) on the grpahics device
-# Plot 1 (upper left)
+# Plot 1 (upper left) - same code as in plot2.R, only the ylab was adjusted
 plot(household_data$Time, household_data$Global_active_power, type="l", xlab="", ylab="Global Active Power")
-# Plot 2 (upper right)
+# Plot 2 (upper right) - new plot, similar to plot 1 (plot2.R)
 plot(household_data$Time, household_data$Voltage, type="l", xlab="datetime", ylab="Voltage")
-# Plot 3 (lower left)
+# Plot 3 (lower left) - same code as in plot 3, only for the legend the line was removed and the legend is slightly smaller
 plot(household_data$Time, household_data$Sub_metering_1, type="n", ylab="Energy sub metering", xlab="")
 points(household_data$Time, household_data$Sub_metering_1, type="l", col="black")
 points(household_data$Time, household_data$Sub_metering_2, type="l", col="red")
 points(household_data$Time, household_data$Sub_metering_3, type="l", col="blue")
 legend("topright", col=c("black", "red", "blue"), legend=colnames(household_data)[7:9], lty=1, bty="n", cex=0.9)
-# Plot 4 (lower right)
+# Plot 4 (lower right) - new plot similar to plot 1 and 2 (plot2.png)
 plot(household_data$Time, household_data$Global_reactive_power, type="l", xlab="datetime", ylab="Globale_reactive_power")
 
 dev.off()
